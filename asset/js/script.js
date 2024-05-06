32601ae5a2ce490f48b10e0c0ff8d8d8c1f7970f
idamkm <idham.mahasin@gmail.com>
2024-05-06 19:15:28 +0700

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,15 @@
+//toggle class active 
+const navbarNav = document.querySelector('.navbar-nav');
+//ketika menu di klik
+document.querySelector('#hamburger-menu').
+    onclick = () => {
+        navbarNav.classList.toggle('active');
+    };
+//klik di luar sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', (e) => {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
